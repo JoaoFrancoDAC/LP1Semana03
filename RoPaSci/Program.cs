75d82097e3d21b6d8731d91631e5a62881e22f05
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-03-06 23:44:54 +0000

    Changed the amount of returns inside of the method

--- RoPaSci/Program.cs
+++ RoPaSci/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Xml.XPath;
 
 namespace RoPaSci
 {
-    public class Program
+    class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int result = RockPaperScissors(args[0], args[1]);
+            switch (result)
+            {
+                case 0:
+                    Console.WriteLine("It's a draw!");
+                    break;
+                case 1:
+                    Console.WriteLine("Player 1 wins!");
+                    break;
+                case 2:
+                    Console.WriteLine("Player 2 wins!");
+                    break;
+            }
+        }
+
+        private static int RockPaperScissors(string player1, string player2)
+        {
+
+            int resultado;
+            if (player1 == player2)
+            {
+                resultado = 0;
+            }
+            if (((player1 == "Rock") && (player2 == "Scissors")) ||
+                ((player1 == "Scissors") && (player2 == "Paper")) ||
+                ((player1 == "Paper") && (player2 == "Rock")))
+            {
+                resultado = 1;
+            }
+            else
+            {
+                resultado = 2;
+            }
+            return resultado;
         }
     }
 }
